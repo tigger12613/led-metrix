238230e1176cc46f3c3033c2c4b2ab3d255ca76f
CcepWang <37070545+CcepWang@users.noreply.github.com>
2019-12-28 15:59:47 +0800

    Add files via upload

---
+++ ball.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   ball.h
+ * Author: MSI
+ *
+ * Created on 2019年12月28日, 下午 2:35
+ */
+
+#ifndef BALL_H
+#define	BALL_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<xc.h>
+int LED[16][16] = {0};
+int left_board[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+int right_board[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+int ball_x=4;
+int ball_y=6;
+int ball_last_x=3;
+int ball_last_y=5;
+
+void initial(void);
+void board(void);
+void ball(void);
+void move_ball(void);
+void Delay(unsigned int secs);
+
+#endif	/* BALL_H */
